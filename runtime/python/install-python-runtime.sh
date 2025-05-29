e946cead8aae4ac9718bb39ec8a9e901747658a5
mada.gugugu <mada.gugugu@bytedance.com>
2025-05-29 18:13:50 +0800

    python3

--- runtime/python/install-python-runtime.sh
+++ runtime/python/install-python-runtime.sh
@@ -13,16 +13,16 @@ python3 -m venv /tmp/sandbox-runtime
 source /tmp/sandbox-runtime/bin/activate
 
 if [ $USE_OFFICIAL_SOURCE -eq 0 ]; then
-    pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+    pip3 config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
 fi
 
-pip install -r ./requirements.txt --ignore-requires-python
+pip3 install -r ./requirements.txt --ignore-requires-python
 
 # for NaturalCodeBench python problem 29
-python -c "import nltk; nltk.download('punkt')"
+python3 -c "import nltk; nltk.download('punkt')"
 
 # for CIBench nltk problems 
-python -c "import nltk; nltk.download('stopwords')"
+python3 -c "import nltk; nltk.download('stopwords')"
 
 pip cache purge
 deactivate
